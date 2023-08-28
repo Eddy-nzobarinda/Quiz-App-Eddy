780bb9d14595206873345588de7ca075a2783f20
eddy nzobarinda <eddynzobarinda910@gmail.com>
2023-08-28 09:20:58 +0200

    integreted the backend registration with the front end form

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import QuizApp from './pages/quizApp';
 import Home from './pages/home';
 import './pages/home.css';
 import Instructions from './pages/instructionPage';
-import './pages/instructionPage.css'
+import './pages/instructionPage.css';
+import Login from './pages/login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUp from './pages/signup';
+import './pages/login.css'
+// import SubmitPage from './pages/submissionPage';
 
 function App() {
   return (
@@ -13,6 +17,9 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/quizApp' element={<QuizApp />} />
           <Route path='/instructionPage' element={<Instructions />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          {/* <Route path='/submissionPage' element={<SubmitPage />} /> */}
         </Routes>
       </Router>
     </>
--- src/pages/quizApp.tsx
+++ src/pages/quizApp.tsx
@@ -1,112 +1,168 @@
-import React,{useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchQuizQuestions } from '../Api';
-//components
 import QuestionCard from '../components/QuestionCard';
-//types
-import { QuestionState,Difficulty } from '../Api';
-//styles
-import'../App.css'
+import { QuestionState, Difficulty } from '../Api';
+import '../App.css';
+import Login from './login';
+import { useNavigate } from 'react-router-dom';
+// import SubmitPage from './submissionPage';
 
 export type AnswerObject = {
-  question : string;
-  answer:string;
+  question: string;
+  answer: string;
   correct: boolean;
   correctAnswer: string;
-} 
-
+}
 
 const TOTAL_QUESTIONS = 50;
-const QuizApp =()=> {
-  const [Loading, setLoading]= useState(false);
-  const [questions, setQuestions]= useState<QuestionState[]>([]);
-  const [number, setNumber]= useState(0);
-  const [userAnswers, setUserAnswers]=useState<AnswerObject[]>([]);
-  const [score, setScore]=useState(0);
-  const [quizOver, setQuizOver]=useState(true)
 
-  
+const QuizApp = () => {
+  const [Loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [quizOver, setQuizOver] = useState(true);
+  const [showScore, setShowScore] = useState(false);
+  const [savedState, setSavedState] = useState({});
+  const [showSubmitPage, setShowSubmitPage] = useState(false);
+
+  const navigate = useNavigate();
 
 
-  const startQuiz = async() =>{
+  useEffect(() => {
+    if (!quizOver) {
+      const dataToSave = {
+        number,
+        userAnswers,
+        quizOver,
+        showScore,
+      };
+      setSavedState(dataToSave);
+      localStorage.setItem('quizAppState', JSON.stringify(dataToSave));
+    }
+  }, [number, userAnswers, quizOver, showScore]);
+
+
+  useEffect(() => {
+    const loadSavedState = async () => {
+      const savedData = localStorage.getItem('quizAppState');
+      if (savedData) {
+        const parsedData = JSON.parse(savedData);
+
+        setNumber(parsedData.number);
+        setUserAnswers(parsedData.userAnswers);
+        setQuizOver(parsedData.quizOver);
+        setShowScore(parsedData.showScore);
+
+        const newQuestions = await fetchQuizQuestions(
+          TOTAL_QUESTIONS,
+          Difficulty.EASY,
+        );
+        setQuestions(newQuestions);
+      }
+    };
+
+    loadSavedState();
+  }, []);
+
+  const startQuiz = async () => {
     setLoading(true);
     setQuizOver(false);
+    setShowScore(false);
     const newQuestions = await fetchQuizQuestions(
       TOTAL_QUESTIONS,
-      Difficulty.EASY
+      Difficulty.EASY,
     );
-    setQuestions(newQuestions)
-    setScore(0)
-    setUserAnswers([])
+    setQuestions(newQuestions);
+    setUserAnswers([]);
     setNumber(0);
-    setLoading(false)
-
+    setLoading(false);
   }
-
-
-  const checkAnswer = (e:React.MouseEvent<HTMLButtonElement>)=>{
-    if(!quizOver){
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!quizOver) {
       //users answer
       const answer = e.currentTarget.value;
       //check answer against correct answer
       const correct = questions[number].correct_answer === answer
-      //add score if answer is correct 
-      if(correct) setScore(prev => prev+1);
       //save answer in the array for user answer
-      const answerObject ={
-        question: questions[number].question,
+      const answerObject = {
+        question: questions[number]?.question,
         answer,
         correct,
         correctAnswer: questions[number].correct_answer,
       };
-      setUserAnswers(prev => [...prev,answerObject])
+      setUserAnswers(prev => [...prev, answerObject])
     }
 
   };
 
-  const nextQuestion = ()=>{
-    //move on to the next question if not the last question
-    const nextQuestion = number +1;
+  const nextQuestion = () => {
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1;
 
-    if(nextQuestion=== TOTAL_QUESTIONS){
-      setQuizOver(true)
-    }else{
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setQuizOver(true);
+      setShowScore(true);
+    } else {
       setNumber(nextQuestion);
     }
+  };
 
-  }
+  const logout = () => {
+    localStorage.removeItem('sessiontoken');
+    setQuizOver(true);
+    setShowScore(false);
+    setUserAnswers([]);
+    setNumber(0);
+    navigate('/login');
+  };
+
+  // const submit = () => {
+  //   setQuizOver(true);
+  //   setShowScore(true);
+  //   setUserAnswers([]);
+  //   setNumber(0);
+  //   navigate('/SubmitPage')
+  // }
+
+  console.log(questions)
   return (
-    <>
-    {/* <GlobalStyle/> */}
     <div className="App">
       <h1>Quiz-App</h1>
-      {quizOver || userAnswers.length === TOTAL_QUESTIONS ?(
-      <button className="start" onClick={startQuiz}>
-        start
-      </button>
-      ):null}
-      {!quizOver ? <p className='score'>score:{score}</p>: null }
+      {quizOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startQuiz}>
+          restart
+        </button>
+      ) : null}
+      {!quizOver && !showScore ? <p className='score'>score:{userAnswers.filter(answer => answer.correct).length}</p> : null}
       {Loading && <p>Loading Questions ......</p>}
-      {!Loading && !quizOver && (
-      < QuestionCard
-      questionNr={number +1}
-      totalQuestion={TOTAL_QUESTIONS}
-      question={questions[number].question}
-      answers={questions[number].answers}
-      userAnswer={userAnswers ? userAnswers[number]: undefined}
-      callback={checkAnswer}
-      />
+      {!Loading && !quizOver && !showScore && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestion={TOTAL_QUESTIONS}
+          question={questions[number]?.question}
+          answers={questions[number]?.answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
       )}
-      {!quizOver &&
-       !Loading && 
-       userAnswers.length === number + 1 &&
-       number !== TOTAL_QUESTIONS -1? (
+      {!quizOver && !Loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
         <button className='next' onClick={nextQuestion}>
           next Question
         </button>
-      ):null}
-      </div>
-      </>
+      ) : null}
+      {/* {showSubmitPage && (
+        <SubmitPage
+          showScore={userAnswers.filter(answer => answer.correct).length}
+        />
+      )} */}
+      {/* <button className="submit" onClick={submit}>
+        submit
+      </button> */}
+      <button className="logout" onClick={logout}>
+        logout
+      </button>
+    </div>
   );
 }
-
 export default QuizApp;
